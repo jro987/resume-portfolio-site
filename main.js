76678f3680594b413f99bacaf8b795ebecb5d8a5
jro987 <jnjurow@gmail.com>
2020-01-21 23:27:33 -0800

    fixed squiggle svg and comments in main js

--- main.js
+++ main.js
@@ -28,10 +28,8 @@ $(window).scroll(function() {
 });
 }).scroll();
 
-// Get the modal
 var modal = document.getElementById("resume-modal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("resume-modal-trigger");
 var modalImg = document.getElementById("img1");
 var captionText = document.getElementById("caption");
@@ -41,10 +39,8 @@ img.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
